b3acf981c3b6d189aa8412a9aeddb1418af639da
Spencer Cooper <spencercooper@gmail.com>
2019-04-27 23:32:11 -0500

    implemented onFormSubmit arrow function

--- 4. pics/src/components/SearchBar.js
+++ 4. pics/src/components/SearchBar.js
@@ -3,16 +3,26 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   state = { term: '' };
 
+  // creating onFormSubmit event handler using an arrow function
+  // ensures the function is automatically bound to `this`
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    console.log(this.state.term);
+  };
+
   render() {
     return (
       <div className='ui segment'>
-        <form className='ui form'>
+        <form onSubmit={this.onFormSubmit} className='ui form'>
           <div className='field'>
             <label>Image Search</label>
             <input
               type='text'
               value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })}
+              onChange={e =>
+                this.setState({ term: e.target.value.toUpperCase() })
+              }
             />
           </div>
         </form>
